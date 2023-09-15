8b3eecf6131efecc4990578ad0ae0002482451a2
Dat Le <ledat1011>
2023-09-15 15:28:32 +0700

    first commit

---
+++ routes/users.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+
+
+let users = [{
+  id: 1,
+  lastName: "Le",
+  firstName: "Dat"
+},
+{
+  id: 2,
+  lastName: "Le",
+  firstName: "A"
+}
+];
+/* GET users listing. */
+router.get('/get-all', function (req, res, next) {
+  res.json(users);
+});
+
+router.get('/get-by-id', function (req, res, next) {
+  let id = req.query.id;
+  let user = users.find(v => v.id == id);
+
+  res.json(user);
+});
+
+module.exports = router;
